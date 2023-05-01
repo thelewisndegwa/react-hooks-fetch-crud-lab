83cc9c602860e71b7d2e0534991e60dc1532eb49
thelewisndegwa <lewiskanyi77@gmail.com>
2023-05-01 16:11:33 +0300

    done

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,93 +1,60 @@
-import React, { useState } from "react";
+import { useState } from 'react';
 
-function QuestionForm(props) {
-  const [formData, setFormData] = useState({
-    prompt: "",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
-    correctIndex: 0,
-  });
+function NewQuestionForm({ addQuestion }) {
+  const [prompt, setPrompt] = useState('');
+  const [answers, setAnswers] = useState(['', '', '', '']);
+  const [correctIndex, setCorrectIndex] = useState(0);
 
-  function handleChange(event) {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(formData);
-  }
+    const newQuestion = { prompt, answers, correctIndex };
+
+    fetch('/questions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newQuestion)
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Add the new question to the QuestionList component state
+        addQuestion(data);
+      })
+      .catch(error => console.error(error));
+  };
+
+  const handleAnswerChange = (e, index) => {
+    const newAnswers = [...answers];
+    newAnswers[index] = e.target.value;
+    setAnswers(newAnswers);
+  };
 
   return (
-    <section>
-      <h1>New Question</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Prompt:
-          <input
-            type="text"
-            name="prompt"
-            value={formData.prompt}
-            onChange={handleChange}
-          />
+    <form onSubmit={handleSubmit}>
+      <label>
+        Prompt:
+        <input type="text" value={prompt} onChange={e => setPrompt(e.target.value)} />
+      </label>
+
+      {answers.map((answer, index) => (
+        <label key={index}>
+          Answer {index + 1}:
+          <input type="text" value={answer} onChange={e => handleAnswerChange(e, index)} />
         </label>
-        <label>
-          Answer 1:
-          <input
-            type="text"
-            name="answer1"
-            value={formData.answer1}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 2:
-          <input
-            type="text"
-            name="answer2"
-            value={formData.answer2}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 3:
-          <input
-            type="text"
-            name="answer3"
-            value={formData.answer3}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 4:
-          <input
-            type="text"
-            name="answer4"
-            value={formData.answer4}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Correct Answer:
-          <select
-            name="correctIndex"
-            value={formData.correctIndex}
-            onChange={handleChange}
-          >
-            <option value="0">{formData.answer1}</option>
-            <option value="1">{formData.answer2}</option>
-            <option value="2">{formData.answer3}</option>
-            <option value="3">{formData.answer4}</option>
-          </select>
-        </label>
-        <button type="submit">Add Question</button>
-      </form>
-    </section>
+      ))}
+
+      <label>
+        Correct answer:
+        <select value={correctIndex} onChange={e => setCorrectIndex(e.target.value)}>
+          {answers.map((answer, index) => (
+            <option key={index} value={index}>{answer}</option>
+          ))}
+        </select>
+      </label>
+
+      <button type="submit">Submit</button>
+    </form>
   );
 }
-
-export default QuestionForm;
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch('/questions')
+      .then(response => response.json())
+      .then(data => setQuestions(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  const handleDropdownChange = (id, value) => {
+    fetch(`/questions/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ correctIndex: value })
+    })
+      .then(() => {
+        const updatedQuestions = questions.map(question => {
+          if (question.id === id) {
+            return { ...question, correctIndex: value };
+          }
+          return question;
+        });
+        setQuestions(updatedQuestions);
+      })
+      .catch(error => console.error(error));
+  };
+
+  const handleDelete = (id) => {
+    fetch(`/questions/${id}`, { method: 'DELETE' })
+      .then(() => {
+        const updatedQuestions = questions.filter(question => question.id !== id);
+        setQuestions(updatedQuestions);
+      })
+      .catch(error => console.error(error));
+  };
+
   return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+    <>
+      <h2>Questions</h2>
+      <ul>
+        {questions.map(question => (
+          <li key={question.id}>
+            <p>{question.prompt}</p>
+            <ul>
+              {question.answers.map((answer, index) => (
+                <li key={index}>{answer}</li>
+              ))}
+            </ul>
+            <button onClick={() => handleDelete(question.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-export default QuestionList;
+function App() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("/questions-060")
+      .then(response => response.json())
+      .then(data => setQuestions(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div className="App">
+      <QuestionList questions={questions} />
+    </div>
+  );
+}
+
+export default App;
